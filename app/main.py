e2eb6fb64b93598d94fdf7239df54c3adae91494
lephuhung <lephuhung77@gmail.com>
2023-09-03 09:36:24 +0700

    update domain

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ logging to app.log
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-thumnail= "https://clmm24h.io/view"
+thumnail= "https://z-image-cdn.com/view"
 '''
 
 http middleware to get IP
