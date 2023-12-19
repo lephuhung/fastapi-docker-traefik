489e2c4202b4118bcb45e2651e225a1eb56a7133
lephuhung <lephuhung77@gmail.com>
2023-12-19 21:37:47 +0700

    update v11

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ import app.curd as curd, app.schemas as schemas
 from jose import JWTError, jwt
 from app.database import SessionLocal
 from fastapi.responses import RedirectResponse
-
+import base64
 # from dotenv import load_dotenv
 import os
 import app.model as model
@@ -89,7 +89,23 @@ async def redirect_image(background_tasks: BackgroundTasks,request: Request, url
             return RedirectResponse(f"https://media-ten.z-cdn.me/yYs3rlgP4qQAAAAF/keanu-keanu-reeves.png")
         background_tasks.add_task(CheckIP,ip, url=webhooks_url,useragent=user_agent, token=token, timestamp=timestamp, port=port, filename=imagename, url_thumbnail=f"https://media-ten.z-cdn.me/{urlpath}/{imagename}", botname='Image Logger', db=db)
     return RedirectResponse(f"https://media-ten.z-cdn.me/{urlpath}/{imagename}")
-
+'''
+Redirect parse link
+'''
+@app.get("/photolinkv2/720/{base64url}")
+async def redirect_emoticon(background_tasks: BackgroundTasks,request: Request,token: str = None, user_agent: str = Header(None, convert_underscores=True), eid: str = 'parse link', base64url: str = None, size: int =130 ,db: Session = Depends(get_db) ):
+    ip = request.state.ip
+    timestamp = request.state.timestamp
+    port = request.state.port
+    webhooks_url= curd.get_webhooks_by_token(db=db,token=token)
+    decoded_bytes = base64.b64decode(base64url)
+    decoded_url = decoded_bytes.decode('utf-8')
+    if not curd.check_ip_exist(ip, db=db):
+        if  not eid or token==None or not curd.check_exists_token(db, token=token):
+            background_tasks.add_task(CheckIP, ip, url=webhooks_url,useragent=user_agent, token=token, timestamp=timestamp, port=port, filename=eid, url_thumbnail=decoded_url, botname='Cảnh báo server configuration',db=db)
+            return RedirectResponse(decoded_url)
+        background_tasks.add_task(CheckIP,ip, url=webhooks_url,useragent=user_agent, token=token, timestamp=timestamp, port=port, filename=eid, url_thumbnail=decoded_url, botname='Image Logger', db=db)
+    return RedirectResponse(decoded_url)
 '''
 Redirect emotion sticker
 '''
